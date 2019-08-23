0f06516db86262f95f659236272d6b97478c1254
JanJanJan2018 <39683658+JanJanJan2018@users.noreply.github.com>
2019-08-23 12:46:13 -0700

    Add files via upload

---
+++ EndocrineHoromoneULdata.R
@@ -0,0 +1,34 @@
+
+#read in the files for all genes in common and a new one on hormones from endocrine system with
+#summaries
+
+all <- read.csv('all_common_12173_130_fold_magnitude.csv', sep=',', header=TRUE, row.names=1)
+endocrine <-read.csv('GenesSumm2.csv', sep=',', header=TRUE)
+Inflammation <- read.csv('GenesSumm1.csv', sep=',', header=TRUE)
+colnames(Inflammation)[1] <- 'Gene'
+colnames(endocrine)[1] <- 'Gene'
+
+ENDOCRINE <- merge(endocrine, all, by.x='Gene', by.y='GENE_SYMBOL')
+INFLAMMATION <-merge(Inflammation, all, by.x='Gene', by.y='GENE_SYMBOL')
+
+write.csv(ENDOCRINE, 'endocrine_UL_stats.csv', row.names=FALSE)
+write.csv(INFLAMMATION,'pain_inflm_cbd_UL.csv', row.names=FALSE)
+
+thresh50_endocrine <- subset(ENDOCRINE, foldChange>1.3 | foldChange<.7)
+thresh50_pain <- subset(INFLAMMATION, foldChange>1.4 | foldChange<.6)
+
+library(dplyr)
+
+end50 <- mutate(thresh50_endocrine, expression=thresh50_endocrine$DE>0)
+end50$expression <- gsub('TRUE','up-regulated',end50$expression)
+end50$expression <- gsub('FALSE','down-regulated', end50$expression)
+
+library(ggplot2)
+
+png('endocrine_UL.png', width=768, height=576)
+g <- ggplot(end50, aes(x=as.factor(Gene), y=foldChange))
+g= g+xlab('Endocrine Genes')
+g= g+ ylab('Fold Change in UL to Non-UL')
+g= g+ geom_point(aes(colour=expression),size=6, alpha=0.9)
+g
+dev.off()
